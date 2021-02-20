638640c786b1f132f2557d1aa8e00c12aec69846
LoganDooley <logan_dooley@brown.edu>
2021-02-20 12:55:39 -0500

    Initial commit with movement and trail components

---
+++ MouseCursor/Assets/move.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public Camera cam;
+    public Rigidbody2D player;
+    public float speed = 1f;
+    public Transform colliders;
+    public int frames_between = 10;
+    private int frame = 0;
+    private Vector3 moveto;
+    private Vector3 mousepos;
+    private List<Transform> mycollide;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveto = new Vector3(0, 1, 0);
+        mousepos = cam.ScreenToWorldPoint(Input.mousePosition);
+        mycollide = new List<Transform>();
+    }
+
+    void FixedUpdate()
+    {
+        frame += 1;
+        if (frame == 10)
+        {
+            frame = 0;
+            mycollide.Add(Instantiate(colliders, player.position, Quaternion.identity));
+            if (mycollide.Count > 22)
+            {
+                mycollide.RemoveAt(0);
+            }
+        }
+        mousepos = cam.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            moveto = new Vector3(mousepos.x, mousepos.y, 0f) - player.transform.position;
+        }
+        player.velocity = moveto.normalized*speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        int index = mycollide.IndexOf(collision.gameObject.transform);
+        if(index != mycollide.Count && index != mycollide.Count - 1 && index != 0 && index != -1)
+        {
+            print(index);
+        }
+    }
+}
